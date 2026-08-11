b0d6ba6c815663a2beb0de1374a26dbd45371a3c
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-08-11 18:07:39 +0100

    Implement message validation and POST endpoint

--- backend/main.mjs
+++ backend/main.mjs
@@ -6,9 +6,70 @@ const port = 4000;
 
 const messages = [];
 
-app.listen(port,()=> {
-    console.error(`Chat server listening on port ${port}`)
+app.listen(port, () => {
+  console.error(`Chat server listening on port ${port}`);
 });
-app.get("/",(req,res) => {
-    res.json(messages)
-})
+app.get("/", (req, res) => {
+  res.json(messages);
+});
+
+function validateBody(body) {
+  if (
+    typeof body != "object" ||
+    body === null ||
+    Array.isArray(body) ||
+    !("message" in body) ||
+    !("username" in body)
+  ) {
+    return "Expected body to be a JSON object containing keys message and username.";
+  }
+  if (typeof body.message !== "string" || typeof body.username !== "string")
+    return "Message and username must be a string.";
+
+  return null;
+}
+function validateMessage(trimmedMessage, trimmedUsername) {
+  if (trimmedMessage.length < 1 || trimmedMessage.length > 500)
+    return "Message must be between 1 and 500 characters.";
+
+  if (trimmedUsername.length < 2 || trimmedUsername.length > 100)
+    return "Username must be between 2 and 100 characters.";
+
+  return null;
+}
+
+app.post("/", (req, res) => {
+  const bodyBytes = [];
+  req.on("data", (chunk) => bodyBytes.push(...chunk));
+  req.on("end", () => {
+    const bodyString = String.fromCharCode(...bodyBytes);
+    let body;
+    try {
+      body = JSON.parse(bodyString);
+    } catch (error) {
+      res.status(400).send("Expected body to be JSON.");
+      return;
+    }
+    const validateBodyError = validateBody(body)
+    if(validateBodyError !==null)
+    {
+        res.status(400).send(validateBodyError);
+        return ;
+    }
+    
+    const message = body.message.trim();
+    const username = body.username.trim();
+    const validateMessageError = validateMessage(message,username);
+    if(validateMessageError !== null)
+    {
+        res.status(400).send(validateMessageError);
+        return;
+    }
+    messages.push({
+        message : message,
+        username: username,
+    });
+    res.status(201).send("Message Added successfully.")
+
+  });
+});
