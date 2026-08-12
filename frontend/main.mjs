9ff987a178f5567e0c1590d8c52d39c69784749a
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-08-12 16:15:55 +0100

    Create message card from HTML template

--- frontend/main.mjs
+++ frontend/main.mjs
@@ -1,17 +1,17 @@
-const backend = "http://localhost:4000/"; 
+const backend = "http://localhost:4000/";
 let messages = [];
 
 async function fetchMessages() {
   try {
-    const response = await fetch(backend);               // sends the HTTP request get by default 
-    if (!response.ok) {                                 
+    const response = await fetch(backend); // sends the HTTP request get by default
+    if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
     messages = await response.json(); //reads the response body and parses JSON into JS value
     sortMessagesOldestFirst(messages);
     // function displayAllMessage(messages){}
-  } catch (error) { 
-    console.error("Failed to fetch messages: ", error)       // what is the error here ????
+  } catch (error) {
+    console.error("Failed to fetch messages: ", error); // what is the error here ????
   }
 }
 
@@ -20,6 +20,23 @@ function sortMessagesOldestFirst(messages) {
     (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
   );
 }
+function MessageCard({ id, username, message, createdAt }) {
+  const template = document.getElementById("show-message-template");
+  const card = template.content.cloneNode(true);
+  //   const root = card.firstElementChild;
+  //         root.id = String(id)
 
+  const usernameEle = card.querySelector(".message-username");
+  usernameEle.textContent = username;
+
+  const messageEle = card.querySelector(".message-content");
+  messageEle.textContent = message;
+
+  const timeEle = card.querySelector(".message-created-time");
+  timeEle.textContent = new Date(createdAt).toLocaleString();
+  timeEle.dateTime = createdAt;
+
+  return card;
+}
 
 fetchMessages();
