5834fd222654735636faa1951b40d29500335f46
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-08-17 15:22:48 +0100

    feat: receive and display WebSocket messages

--- frontend/websocket.mjs
+++ frontend/websocket.mjs
@@ -1,7 +1,33 @@
+import {
+  sortMessagesOldestFirst,
+  validateMessage,
+  addMessageIfNew,
+  MessageCard,
+  renderMessages,
+} from "./shared.mjs";
+
 const websocketURL = "ws://localhost:4000";
 const websocket = new WebSocket(websocketURL, "chat-protocol");
+const rootEle = document.getElementById("messages-root")
+const submitButton = document.getElementById("submit-button");
+const formFeedbackMessage = document.getElementById("form-feedback");
 
-const submitButton =document.getElementById("submit-button")
+let messages = [];
+
+function handleReceivedMessage(receivedObject) {
+  if (receivedObject.type === "message-history") {
+    messages = sortMessagesOldestFirst(receivedObject.data);
+    renderMessages(messages);
+  }
+  if (receivedObject.type === "message-added") {
+    const newMessage = receivedObject.data;
+    console.log(newMessage);
+    addMessageIfNew(messages, newMessage);
+  }
+  if (receivedObject.type === "error") {
+    formFeedbackMessage.textContent = receivedObject.data;
+  }
+}
 
 websocket.addEventListener("open", () => {
   console.log("connected to the websocket server");
@@ -17,5 +43,8 @@ websocket.addEventListener("close", () => {
   //I can add the connection status (disconnected) and add className error
 });
 websocket.addEventListener("message", (event) => {
-  //here event is the message I received form backend
+const receivedObject= JSON.parse(event.data)
+handleReceivedMessage(receivedObject)
 });
+
+
